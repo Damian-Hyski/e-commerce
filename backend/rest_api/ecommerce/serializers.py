904f79b352dfe83731bd122920ba04a5711f9f22
Hysek <d.hyski@gmail.com>
2023-11-24 11:06:33 +0100

    ProductReview related for Product

--- backend/rest_api/ecommerce/serializers.py
+++ backend/rest_api/ecommerce/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Product
+from .models import Product, ProductReview
+
+class ProductReviewSerializer(ModelSerializer):
+    class Meta:
+        model = ProductReview
+        fields = '__all__'
 
 class ProductSerializer(ModelSerializer):
+    reviews = ProductReviewSerializer(many=True, read_only=True)
+
     class Meta:
         model = Product
         fields = '__all__'
+        
