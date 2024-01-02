0e85fc13c2534e85ebb4681be9ff48bfe716403a
Hysek <d.hyski@gmail.com>
2024-01-02 09:48:35 +0100

    Created endpoint POST for User Authenticate

--- backend/rest_api/ecommerce/urls.py
+++ backend/rest_api/ecommerce/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login', views.login),
     path('products/', views.products),
     path('products/<int:product_id>', views.product_detail),
     path('products/<int:product_id>/reviews', views.reviews),
--- backend/rest_api/ecommerce/views.py
+++ backend/rest_api/ecommerce/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer, ProductReviewSerializer
 
+# User
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        return Response({'message': 'Pomyślnie zalogowano'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'message': 'Nieprawidłowe dane logowania'}, status=status.HTTP_401_UNAUTHORIZED)
+
+# Product
 @api_view(['GET', 'POST'])
 def products(request):
     if request.method == 'GET':
@@ -40,7 +54,8 @@ def product_detail(request, product_id):
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
+# Review
 @api_view(['GET', 'POST'])
 def reviews(request, product_id):
     try:
